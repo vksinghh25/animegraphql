31e798bae3489f2c3341be97e176c4daca3ade81
Vipan K Singh <vksinghh@Vipans-MacBook-Pro.local>
2019-08-27 14:08:44 +0530

    More Stuff.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
 import Details  from './Details.js';
 import SingleGridComponent from './SingleGridComponent.js';
 
@@ -58,31 +57,59 @@ class App extends React.Component {
       })
   }
 
-  // arrow function is now the preferred way to write functions inside the class
-  // async/await is just a better way of writing the promise based code
-  // it helps you write and visualise asyncronous code in sync manner
-  getAnime = async (query, variables) => {
-    try {
-      const response = await axios.post('https://graphql.anilist.co', {
-        query,
-        variables
-      });
+  componentDidUpdate(prevProps, prevState) {
+    // This is the GraphQL query
+    if(!this.state.isLoaded) {
+      const query = `
+      query {
+        Page {
+          media(isAdult: false, sort: POPULARITY_DESC) {
+            id
+            title {
+              romaji
+              english
+            }
+            coverImage {
+              large
+            }
+          }
+        }
+      }
+      `;
 
-      // Log the response so we can look at it in the console
-      console.log(response.data)
-
-      // either this would be set into the state
-      // or the error would be set into the state
-      // whenever that happens, the render function is called
-      // Set the data to the state
-      this.setState(() => ({
-        isLoaded: true,
-        items: response.data.data.Page.media
-      }));
-    } catch (error) {
-      // If there's an error, set the error to the state
-      this.setState(() => ({ error }))
+      // These variables are optional, leave empty for now
+      // const variables = {};
+      // We call the method here to execute our async function
+      // this.getAnime(query, variables)
+      // just for kicks, let's do this using fetch as  well
+      fetch('https://graphql.anilist.co/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: query,
+          }),
+        })
+        .then(response => {
+          return response.json()
+        })
+        .then(responseAsJson => {
+          this.setState({ isLoaded: true, items: responseAsJson.data.Page.media });
+        })
     }
+  }
+
+  backHandler = () => {
+    this.setState({
+        error: null,
+        isLoaded: false,
+        items: [],
+        currentPage: 'grid',
+        selectedTitle: '',
+        selectedTitleUrl: ''
+      }
+    );
   }
 
   clickHandler = (title, url, id) => {
@@ -110,7 +137,7 @@ class App extends React.Component {
       );
       }
     } else if(currentPage === 'details') {
-      return <Details title={selectedTitle} url={selectedTitleUrl} id={selectedId}/>;
+      return <Details title={selectedTitle} url={selectedTitleUrl} id={selectedId} backHandler={this.backHandler}/>;
     }
 
   }
--- src/Details.js
+++ src/Details.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './Details.css';
+import { Button, Popover, OverlayTrigger, Image, Row } from 'react-bootstrap';
 
 class Details extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: []
+      item: [],
+      popoverOpen: false,
+      isLoaded: false
     }
   }
 
+  // arrow function
+  toggle = () => {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   componentDidMount() {
+    console.log("Inside componentDidMount");
     //this time we load the id that has been chosen
     fetch('https://graphql.anilist.co/', {
         method: 'POST',
@@ -22,9 +33,24 @@ class Details extends React.Component {
               Page {
                 media(id: $id) {
                   id
+                  meanScore
+                  genres
                   title {
                     romaji
                     english
+                  }
+                  characters {
+                    nodes {
+                      id
+                      description(asHtml: false)
+                      image {
+                        medium
+                      }
+                      name {
+                        full
+                        native
+                      }
+                    }
                   }
                   description(asHtml: false)
                   countryOfOrigin
@@ -37,7 +63,7 @@ class Details extends React.Component {
             }
           `,
           variables: {
-            count: parseInt(this.props.id),
+            id: parseInt(this.props.id),
           },
         }),
       })
@@ -46,25 +72,64 @@ class Details extends React.Component {
       })
       .then(responseAsJson => {
         console.log(responseAsJson.data);
-        this.setState({item: responseAsJson.data.Page.media});
-      })
+        this.setState({item: responseAsJson.data.Page.media, isLoaded: true});
+      });
   }
 
   render() {
-    return(
-      <div>
-        <div className="split left">
-            <img src={this.props.url} alt={this.props.title}/>
-        </div>
-        <div className="split right">
-          <div className="centered vertically">
-            <h1> {this.props.title} </h1>
-            <p> {this.state.item[0] && this.state.item[0].description} </p>
-            <p> {this.state.item[0] && this.state.item[0].countryOfOrigin} </p>
+    console.log("Calling Details with id : " + this.props.id);
+    if (!this.state.isLoaded) {
+      return <div>Loading...</div>;
+    }
+    else {
+      return(
+          <section className="intro">
+          <Row>
+            <div className="centered vertically">
+              <h1> {this.props.title} </h1>
+              <p> {this.state.item[0] && 'Score: ' + this.state.item[0].meanScore + '/100'} </p>
+              <p> {this.state.item[0] && this.state.item[0].description} </p>
+              <p><b> {this.state.item[0] && 'Country of Origin:  ' + this.state.item[0].countryOfOrigin} </b></p>
+              <p>
+                {this.state.item[0] && this.state.item[0].genres.map(genre => (
+                  <span key={genre} className="badge badge-secondary">{genre}</span>
+                ))}
+              </p>
+            </div>
+          </Row>
+          <Row>
+          <div className="col-lg-6 col-sm-12 left">
+              <img className="stretch" src={this.props.url} alt={this.props.title}/>
           </div>
-        </div>
-      </div>
-    );
+
+          <div className="col-lg-6 col-sm-12 right">
+
+            <div className="centered vertically">
+              <p>
+                {this.state.item[0] && this.state.item[0].characters.nodes.map(node => (
+                    <span key={node.id}>
+                    <OverlayTrigger trigger="hover" placement="top" overlay={
+                      <Popover id={node.id}>
+                        <Popover.Title as="h3">{node.name.native}</Popover.Title>
+                        <Popover.Content>
+                          {node.name.full}
+                        </Popover.Content>
+                      </Popover>
+                    }>
+                      <Image className="tiny" id={node.id} alt={node.name.full} src={node.image.medium} thumbnail/>
+                    </OverlayTrigger>
+                    </span>
+                ))}
+              </p>
+
+              <p> {this.state.item[0] && <Button type="button" className="btn btn-primary" onClick={this.props.backHandler}>Back</Button>}</p>
+            </div>
+
+          </div>
+        </Row>
+        </section>
+      );
+    }
   }
 }
 
